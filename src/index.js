cf2d2dad09c6116c2dee314ac9191b239f53285f
mateuslph <mlptche@gmail.com>
2024-07-03 19:36:59 -0300

    refatorado index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+  rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+  crossorigin="anonymous">
+</link>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
