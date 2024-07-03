1a872c94a4f1c31a02ed87e4d311a4826b8cd6f8
mateuslph <mlptche@gmail.com>
2024-07-03 19:41:22 -0300

    refatora tabela.js

--- src/Tabela/Tabela.js
+++ src/Tabela/Tabela.js
@@ -1,34 +1,41 @@
 function Tabela({ vetor, selecionar }) {
     return (
-        <table className="table ">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Nome</th>
-                    <th>Marca</th>
-                    <th>Selacionar</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    vetor.map((obj, indice) => (
-                        <tr key={indice}>
-                            <td>{indice+1}</td>
-                            <td>{obj.nome}</td>
-                            <td>{obj.marca}</td>
-                            <td>
-                                <button
-                                    onClick={() => {selecionar(indice)}}
-                                    className="btn btn-success"
-                                >
-                                    Selecionar
-                                </button>
-                            </td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
+        <div className="borda-tabela">
+            <div>
+                <div>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nome</th>
+                                <th>Marca</th>
+                                <th>Selacionar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                vetor.map((obj, indice) => (
+                                    <tr key={indice}>
+                                        <td>{indice + 1}</td>
+                                        <td>{obj.nome}</td>
+                                        <td>{obj.marca}</td>
+                                        <td>
+                                            <button
+                                                id="botao-selecionar"
+                                                onClick={() => { selecionar(indice) }} // Chamando selecionar com o índice
+                                                className="btn btn-success"
+                                            >
+                                                Selecionar
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     )
 }
 
