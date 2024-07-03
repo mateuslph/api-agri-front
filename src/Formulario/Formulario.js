a6d069ae260628e00bb273a16b70c639616388fa
mateuslph <mlptche@gmail.com>
2024-07-03 19:35:23 -0300

    refatora formulario.js

--- src/Formulario/Formulario.js
+++ src/Formulario/Formulario.js
@@ -1,52 +1,60 @@
 function Formulario({ botao, eventoTeclado, cadastrar,
     obj, cancelar, remover, alterar }) {
     return (
-        <form>
-            <input
-                type="text"
-                value={obj.nome}
-                onChange={eventoTeclado}
-                name="nome"
-                placeholder="Nome"
-                className="form-control"
-            />
-            <input
-                type="text"
-                value={obj.marca}
-                onChange={eventoTeclado}
-                name="marca"
-                placeholder="Marca"
-                className="form-control"
-            />
-            {
-                botao
-                    ? <input
-                        type="button"
-                        value="Cadastrar"
-                        onClick={cadastrar}
-                        className="btn btn-primary" />
-                    : <div>
+        <div className="borda-formulario">
+            <div>
+                <div>
+                    <form className="form">
                         <input
-                            type="button"
-                            value="Alterar"
-                            onClick={alterar}
-                            className="btn btn-warning"
+                            type="text"
+                            value={obj.nome}
+                            onChange={eventoTeclado}
+                            name="nome"
+                            placeholder="Nome"
+                            className="form-control"
+                            maxLength="50"
                         />
                         <input
-                            type="button"
-                            value="Remover"
-                            onClick={remover}
-                            className="btn btn-danger"
+                            type="text"
+                            value={obj.marca}
+                            onChange={eventoTeclado}
+                            name="marca"
+                            placeholder="Marca"
+                            className="form-control"
+                            maxLength="50"
                         />
-                        <input
-                            type="button"
-                            value="Cancelar"
-                            onClick={cancelar}
-                            className="btn btn-secondary"
-                        />
-                    </div>
-            }
-        </form>
+                        {
+                            botao
+                                ? <input
+                                    type="button"
+                                    value="Cadastrar"
+                                    onClick={cadastrar}
+                                    className="btn btn-primary" />
+                                : <div>
+                                    <input
+                                        type="button"
+                                        value="Alterar"
+                                        onClick={alterar}
+                                        className="btn btn-warning"
+                                    />
+                                    <input
+                                        type="button"
+                                        value="Remover"
+                                        onClick={remover}
+                                        className="btn btn-danger"
+                                    />
+                                    <input
+                                        type="button"
+                                        value="Cancelar"
+                                        onClick={cancelar}
+                                        className="btn btn-secondary"
+                                    />
+                                </div>
+                        }
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
