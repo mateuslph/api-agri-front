a04f2c805ad395997a028c8b17f8bd86a70d1bfe
mlptche <mlptche@gmail.com>
2025-04-12 21:54:20 +0000

    refatora separando os elementos da pagina

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Footer from './Footer/Footer';
 import Formulario from './Formulario/Formulario';
-import Menu from './Menu';
+import Menu from './Menu/Menu';
 import ProductList from './ProductList/ProductList';
 import useProductManagement from './hooks/useProductManagement';
 
