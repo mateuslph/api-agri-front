d648f2ae7198934d391d3dc7367bd454d5573de6
mlptche <mlptche@gmail.com>
2025-04-12 23:51:34 +0000

    estiliza o formulario

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Menu.css'; // Update: The path is now relative within the Components directory
+import './Menu.css';
 
 function Menu() {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
